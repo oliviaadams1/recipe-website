cdb165e1b075ae784d7287e3aea32b5e37f7844b
oliviaadams1 <125403532+oliviaadams1@users.noreply.github.com>
2023-06-08 12:08:44 -0500

    Adding website files

---
+++ script.js
@@ -0,0 +1,27 @@
+function userHover() {
+  document.getElementById(".dropbtn");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onmouseover = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
